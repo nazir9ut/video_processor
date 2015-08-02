efdff0ddd0080406bf45f3fb56c52f85667c975b
YOUR NAME <YOUR@EMAIL.com>
2015-08-02 16:17:23 +0600

    commit

--- process.py
+++ process.py
@@ -1,41 +1,41 @@
-import subprocess
-import numpy as np
+import os
+import fnmatch
+import time
+
 from db_settings import *
 import db_helpers
 import ffmpeg_helpers
 import string_helpers
 import path_helpers
-import glob
-import os
-import fnmatch
-import time
+
+
+
+
+
+
+ffmpeg_path = '/home/zhuma/bin/ffmpeg'
+
+
+base_path = '/media/zhuma/6111-781C/Videodata'
+src_ext = '.vid'
+dst_ext = '.webm'
+src_folder = "Videodata"
+dst_folder = "video_compressed"
+
+
 
 
 
 db_helpers.db_tables_init()
 
-ffmpeg_path = '/home/zhuma/bin/ffmpeg'
-
-
-
-
-# /media/zhuma/6111-781C/Videodata/10-07-2015/cam0/401.vid
-
-
-
-base_path = '/media/zhuma/6111-781C/Videodata'
-
-
 
 matches = []
 for root, dirnames, filenames in os.walk(base_path):
-    filtered = fnmatch.filter(filenames, '*.vid')
+    filtered = fnmatch.filter(filenames, '*' + src_ext)
 
     # at least 2 files with .vid extension guarantees
-    # that one of them has finished recording video
-    # so we can perform compression on it
+    # that at least one of them has finished recording video
     if len(filtered) >= 2:
-
 
         filtered = sorted(filtered, key = lambda filename: time.ctime(os.path.getmtime(os.path.join(root, filename))))
 
@@ -44,86 +44,14 @@ for root, dirnames, filenames in os.walk(base_path):
         for filename in filtered:
             src_file = os.path.join(root, filename)
             exists = VideoModel.select().where(VideoModel.path_and_file == src_file).exists()
+
             if not exists:
-                # matches.append(file)
+                dst_dir = path_helpers.create_dst_dirs(src_file, src_folder, dst_folder)
 
-                dst_dir = path_helpers.create_dst_dirs(src_file, "Videodata", "video_compressed")
-
-                dst_file = path_helpers.get_dst_file(src_file, dst_dir, '.webm')
+                dst_file = path_helpers.get_dst_file(src_file, dst_dir, dst_ext)
 
                 ffmpeg_helpers.first_and_second_pass(ffmpeg_path, src_file, dst_file, dst_dir)
 
 
 
-
-
-# matches = np.array(matches)
-
-
-# for key, match in enumerate(matches):
-#     # print(match)
-#     exists = VideoModel.select().where(VideoModel.path_and_file == match).exists()
-#     print(exists)
-#     if(exists):
-#         print(match)
-#         print(key)
-#         # del matches[key]
-
-
-
-
-
-
-
-# for src_file in matches:
-#     print(src_file)
-#
-#     # src_file = '/media/zhuma/6111-781C/Videodata/10-07-2015/cam0/402.vid'
-#
-#
-#     dst_dir = path_helpers.create_dst_dirs(src_file, "Videodata", "video_compressed")
-#
-#
-#     dst_file = path_helpers.get_dst_file(src_file, dst_dir, '.webm')
-#
-#
-#     ffmpeg_helpers.first_and_second_pass(ffmpeg_path, src_file, dst_file, dst_dir)
-
-
-
-
-
-
-    # # 1st pass
-    # ffmpeg_helpers.first_pass(ffmpeg_path, src_file)
-    #
-    # print('dddddddddddddddddddddddddddddddd')
-    #
-    # # 2nd pass
-    # dst_file = '/home/naz/PycharmProjects/video_processor/videos_src/470.webm'
-    # ffmpeg_helpers.second_pass(ffmpeg_path, src_file, dst_file)
-
-
-# files = glob.glob(os.path.join(src_path, '*.vid'))
-
-
-
-# for src_file in files:
-#     pass
-    # print(src_file)
-
-    # print(string_helpers.get_ext(src_file))
-    # print(string_helpers.get_file_name(src_file))
-    #
-    # print(string_helpers.get_last_segment(src_file))
-
-    # # 1st pass
-    # ffmpeg_helpers.first_pass(ffmpeg_path, src_file)
-    #
-    #
-    # dst_file = os.path.join(dst_path, 'out1.webm')
-    #
-    #
-    # # 2nd pass
-    # ffmpeg_helpers.second_pass(ffmpeg_path, src_file, dst_file)
 
