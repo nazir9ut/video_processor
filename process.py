3346614986f8436e44018e657980ba6f8ad65258
YOUR NAME <YOUR@EMAIL.com>
2015-08-02 16:02:59 +0600

    commit

--- process.py
+++ process.py
@@ -42,10 +42,16 @@ for root, dirnames, filenames in os.walk(base_path):
         # del filtered[-1]
 
         for filename in filtered:
-            file = os.path.join(root, filename)
-            exists = VideoModel.select().where(VideoModel.path_and_file == file).exists()
+            src_file = os.path.join(root, filename)
+            exists = VideoModel.select().where(VideoModel.path_and_file == src_file).exists()
             if not exists:
-                matches.append(file)
+                # matches.append(file)
+
+                dst_dir = path_helpers.create_dst_dirs(src_file, "Videodata", "video_compressed")
+
+                dst_file = path_helpers.get_dst_file(src_file, dst_dir, '.webm')
+
+                ffmpeg_helpers.first_and_second_pass(ffmpeg_path, src_file, dst_file, dst_dir)
 
 
 
@@ -69,19 +75,19 @@ for root, dirnames, filenames in os.walk(base_path):
 
 
 
-for src_file in matches:
-    print(src_file)
-
-    # src_file = '/media/zhuma/6111-781C/Videodata/10-07-2015/cam0/402.vid'
-
-
-    dst_dir = path_helpers.create_dst_dirs(src_file, "Videodata", "video_compressed")
-
-
-    dst_file = path_helpers.get_dst_file(src_file, dst_dir, '.webm')
-
-
-    ffmpeg_helpers.first_and_second_pass(ffmpeg_path, src_file, dst_file, dst_dir)
+# for src_file in matches:
+#     print(src_file)
+#
+#     # src_file = '/media/zhuma/6111-781C/Videodata/10-07-2015/cam0/402.vid'
+#
+#
+#     dst_dir = path_helpers.create_dst_dirs(src_file, "Videodata", "video_compressed")
+#
+#
+#     dst_file = path_helpers.get_dst_file(src_file, dst_dir, '.webm')
+#
+#
+#     ffmpeg_helpers.first_and_second_pass(ffmpeg_path, src_file, dst_file, dst_dir)
 
 
 
