260cc4cb9b1eeb4036033dd9cc0cbca847b477fb
YOUR NAME <YOUR@EMAIL.com>
2015-08-02 15:46:32 +0600

    commit

--- process.py
+++ process.py
@@ -4,6 +4,7 @@ from db_settings import *
 import db_helpers
 import ffmpeg_helpers
 import string_helpers
+import path_helpers
 import glob
 import os
 import fnmatch
@@ -11,13 +12,14 @@ import time
 
 
 
-db_helpers.db_table_init()
+db_helpers.db_tables_init()
 
 ffmpeg_path = '/home/zhuma/bin/ffmpeg'
 
 
 
 
+# /media/zhuma/6111-781C/Videodata/10-07-2015/cam0/401.vid
 
 
 
@@ -33,28 +35,59 @@ for root, dirnames, filenames in os.walk(base_path):
     # that one of them has finished recording video
     # so we can perform compression on it
     if len(filtered) >= 2:
+
+
         filtered = sorted(filtered, key = lambda filename: time.ctime(os.path.getmtime(os.path.join(root, filename))))
 
-        del filtered[-1]
-
+        # del filtered[-1]
 
         for filename in filtered:
-            matches.append(os.path.join(root, filename))
+            file = os.path.join(root, filename)
+            exists = VideoModel.select().where(VideoModel.path_and_file == file).exists()
+            if not exists:
+                matches.append(file)
 
 
 
 
 
+# matches = np.array(matches)
+
+
+# for key, match in enumerate(matches):
+#     # print(match)
+#     exists = VideoModel.select().where(VideoModel.path_and_file == match).exists()
+#     print(exists)
+#     if(exists):
+#         print(match)
+#         print(key)
+#         # del matches[key]
 
 
 
 
-matches = np.array(matches)
 
 
 
 for src_file in matches:
     print(src_file)
+
+    # src_file = '/media/zhuma/6111-781C/Videodata/10-07-2015/cam0/402.vid'
+
+
+    dst_dir = path_helpers.create_dst_dirs(src_file, "Videodata", "video_compressed")
+
+
+    dst_file = path_helpers.get_dst_file(src_file, dst_dir, '.webm')
+
+
+    ffmpeg_helpers.first_and_second_pass(ffmpeg_path, src_file, dst_file, dst_dir)
+
+
+
+
+
+
     # # 1st pass
     # ffmpeg_helpers.first_pass(ffmpeg_path, src_file)
     #
--- process_one.py
+++ process_one.py
@@ -20,16 +20,10 @@ db_helpers.db_tables_init()
 src_file = '/media/zhuma/6111-781C/Videodata/10-07-2015/cam0/402.vid'
 
 
-
-
-
 dst_dir = path_helpers.create_dst_dirs(src_file, "Videodata", "video_compressed")
 
 
 dst_file = path_helpers.get_dst_file(src_file, dst_dir, '.webm')
-
-
-
 
 
 ffmpeg_helpers.first_and_second_pass(ffmpeg_path, src_file, dst_file, dst_dir)
