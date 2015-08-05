eceee4a45793a591dd0b08764f75657bc775f567
naz <nazir8ut@gmail.com>
2015-08-05 16:06:29 +0600

    vlc trans

--- process_bad.py
+++ process_bad.py
@@ -54,23 +54,27 @@ print(final_dst_dir)
 
 vlc_dst_file = os.path.join('/tmp/', file_name + tmp_name_postfix)
 
+
+
 if vlc_helpers.transcode(src_file, vlc_dst_file):
     print('111')
-    # ff_src_file = vlc_dst_file
-    # ff_dst_dir = path_helpers.create_dst_dirs(src_file, src_folder, dst_folder)
-    # ff_dst_file = path_helpers.get_dst_file(ff_src_file, ff_dst_dir, dst_ext)
-    #
-    # if ffmpeg_helpers.first_and_second_pass_bad(ffmpeg_path, ff_src_file, ff_dst_file):
-    #     final_dst_file = ff_dst_file
-    # else:
-    #     final_dst_file = os.path.join(final_dst_dir, file_name + src_ext) #  /media/naz/6111-781C/video_compressed/10-07-2015/cam0/427.vid
-    #     # copy file to dst as is
-    #     shutil.copy(src_file, final_dst_dir)
+    ff_src_file = vlc_dst_file
+    ff_dst_dir = path_helpers.create_dst_dirs(src_file, src_folder, dst_folder)
+    ff_dst_file = path_helpers.get_dst_file(ff_src_file, ff_dst_dir, dst_ext)
+
+    if ffmpeg_helpers.first_and_second_pass_bad(ffmpeg_path, ff_src_file, ff_dst_file):
+        print('111.1')
+        final_dst_file = ff_dst_file
+    else:
+        print('000.0')
+        final_dst_file = os.path.join(final_dst_dir, file_name + src_ext) #  /media/naz/6111-781C/video_compressed/10-07-2015/cam0/427.vid
+        # copy file to dst as is
+        shutil.copy(src_file, final_dst_dir)
 else:
     print('000')
-    # final_dst_file = os.path.join(final_dst_dir, file_name + src_ext) #  /media/naz/6111-781C/video_compressed/10-07-2015/cam0/427.vid
-    # # copy file to dst as is
-    # shutil.copy(src_file, final_dst_dir)
+    final_dst_file = os.path.join(final_dst_dir, file_name + src_ext) #  /media/naz/6111-781C/video_compressed/10-07-2015/cam0/427.vid
+    # copy file to dst as is
+    shutil.copy(src_file, final_dst_dir)
 
 
 
@@ -82,14 +86,14 @@ else:
 
 
 
-# with db.atomic() as txn:
-#     bad_file_model.is_resolved = True
-#     bad_file_model.save()
-#
-#     try:
-#         VideoModel.create(path_and_file=bad_file_model.path_and_file)
-#     except IntegrityError:
-#         print("IntegrityError: src_file exists in DB")
+with db.atomic() as txn:
+    bad_file_model.is_resolved = True
+    bad_file_model.save()
+
+    try:
+        VideoModel.create(path_and_file=bad_file_model.path_and_file)
+    except IntegrityError:
+        print("IntegrityError: src_file exists in DB")
 
 
 
